ea48c1207b6450b42f2f6b19adc13e260e798d95
heckgustavo <heckgustavoluiz@gmail.com>
2023-04-14 13:41:27 +0000

    Alterando mensagem inicial

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello I like to make AI Apps'
+    return 'Hello world de CD com GCP'
 
 @app.route('/name/<value>')
 def name(value):
